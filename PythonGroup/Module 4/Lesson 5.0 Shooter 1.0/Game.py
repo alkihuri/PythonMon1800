bbf4a4698f9050c17b1b30c77269a1351b6519f8
chelovek957 <tremagsis@gmail.com>
2022-04-11 19:10:54 +0300

    lifecycleStart

--- PythonGroup/Module 4/Lesson 5.0 Shooter 1.0/Game.py
+++ PythonGroup/Module 4/Lesson 5.0 Shooter 1.0/Game.py
@@ -5,7 +5,13 @@ print("Robolab Python Pro Course / Shooter template project =) ")
 
 
 # modules import 
+import pygame
+import random
+from os import path
 
+WIDTH=480
+HEIGHT=600
+FPS=15
 
 
 
@@ -22,17 +28,50 @@ print("Robolab Python Pro Course / Shooter template project =) ")
 
 
 # Window creating
+pygame.init()
+pygame.mixer.init()
+screen=pygame.display.set_caption((WIDTH,HEIGHT))
+pygame.display.set_caption("Shooter")
+clock=pygame.time.Clock()
 
 
 
-
- 
+img_dir=path.join(path.dirname(__file__), 'img') 
 # Game sprites 
+background=pygame.image.load(path.join(img_dir, "field.png")).convert()
+background_rect=background.get_rect()
+player_img=pygame.image.load(path.join(img_dir, "ship.png")).convert()
+npc_img=pygame.image.load(path.join(img_dir, "npc.png")).convert()
+bullet_img=pygame.image.load(path.join(img_dir,"bullet.png")).convert()
+
+
 
 
 
 #player class
- 
+class Player(pygame.sprite.Sprite):
+    score=0
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.transform.scale(player_img,(50,50))
+        self.image.set_colorkey(WHITE)
+        self.rect=self.image.get_rect()
+        self.rect.centrex=WIDTH/2
+        self.rect.bottom=HEIGHT-10
+        self.speedx=0
+
+    def update(self):
+        self.speedx=0
+        keystate=pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx=-8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx=8
+        self.rect.x+=self.speedx
+        if self.rect.right>WIDTH:
+            self.rect.right=WIDTH
+        if self.rect.left<0:
+            self.rect.left=0
 
 
 
--- PythonGroup/Module 4/Lesson 6.0 Shooter 2.0/Game.py
+++ PythonGroup/Module 4/Lesson 6.0 Shooter 2.0/Game.py
@@ -5,6 +5,7 @@ print("Robolab Python Pro Course / Shooter template project =) ")
 
 
 # modules import 
+from re import M
 import pygame
 import random
 from os import path
@@ -23,11 +24,11 @@ pygame.display.set_caption("Shooter 2nd lesson")
 clock = pygame.time.Clock() 
 # Game sprites 
 img_dir = path.join(path.dirname(__file__),"img")
-background = pygame.load(path.join(img_dir,"field.png")).convert()
+background = pygame.image.load(path.join(img_dir,"field.png")).convert()
 background_rect = background.get_rect() 
-player_img = pygame.load(path.join(img_dir,"field.png")).convert()
-npc_img = pygame.load(path.join(img_dir,"npc.png")).convert()
-bullet_img = pygame.load(path.join(img_dir,"bullet.png")).convert()
+player_img = pygame.image.load(path.join(img_dir,"field.png")).convert()
+npc_img = pygame.image.load(path.join(img_dir,"npc.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir,"bullet.png")).convert()
 
 #player class
 class Player(pygame.sprite.Sprite):
@@ -54,14 +55,51 @@ class Player(pygame.sprite.Sprite):
 
 #mob generation system ~ mob (NPC) class
 
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.transform.scale(npc_img, (50,50))
+        self.image.set_colorkey(WHITE)
+        self.rect=self.image.get_rect()
+        self.rect.x=random.randrange(WIDTH-self.rect.width)
+        self.rect.y=random.randrange(-300,-30)
+        self.speedy=random.randrange(1,8)
+        self.speedx=random.randrange(-3,3)
+
+    def update(self):
+        self.rect.x+=self.speedx
+        self.rect.y+=self.speedy
+        if self.rect.top>HEIGHT+10 or self.rect.left < -25 or self.rect.right > WIDTH+20:
+            self.rect.x=random.randrange(WIDTH-self.rect.width)
+            self.rect.y=random.randrange(-100,-40)
+            self.speedy=random.randrange(1,8)
 #mob generation system ~ generation
 
+all_sprites=pygame.sprite.Group()
+mobs=pygame.sprite.Group()
+bullets=pygame.sprite.Group()
+player=Player()
+all_sprites.add(player)
+
+for i in range(2):
+    m=Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
 #Game lifecycle
+running=True
+while running:
     #fps
-
+    clock.tick(FPS)
     #event handler
+    for event in pygame.event.get():
+    #event to close window
 
-        #event to close window
+        if event.type==pygame.QUIT:
+            running=False
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                print("Shoot")
 
         #shoot [if event.type == pygame.KEYDOWN] [if event.key == pygame.K_SPACE]
 
