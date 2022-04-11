cba077590259a9245eda41674e01659cc44472ff
SaveliyFrolikov <thecoder13oops@gmail.com>
2022-04-11 18:22:04 +0300

    Shooter

--- PythonGroup/Module 4/Lesson 5.0 Shooter 1.0/Game.py
+++ PythonGroup/Module 4/Lesson 5.0 Shooter 1.0/Game.py
@@ -5,34 +5,48 @@ print("Robolab Python Pro Course / Shooter template project =) ")
 
 
 # modules import 
+import pygame
+import random
+from os import path
 
-
-
-
-
-
+WIDTH = 480
+HEIGHT = 600
+FPS = 15
 
 # colors set up
-
-
-
-
-
-
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELOW = (255, 255, 0)
 
 
 # Window creating
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_function("Shooter")
+clock = pygame.time.Clock()
 
+img_dir = path.join(path.dirname(__file__), "img")
 
-
-
- 
 # Game sprites 
-
-
+backround = pygame.image.load(path.join(img_dir, "field.png")).convert()
+backround_rect = backround.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "ship.png")).convert()
+npc_img = pygame.image.load(path.join(img_dir, "npc.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "bullet.png")).convert()
 
 #player class
- 
+ class Player(pygame.sprite.Sprite):
+     score = 0
+
+     def __init__(self):
+         pygame.sprite.Sprite__init__(self)
+         self.image = pygame.transform.scale(player_img, (50, 50))
+         self.image.set_colorkey(WHITE)
+         self.image.
 
 
 
