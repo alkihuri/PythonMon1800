5b7d4a912fb02238a5d4c03cf11bbec3fcf19010
chelovek957 <tremagsis@gmail.com>
2022-05-23 19:22:29 +0300

    PyQt001

--- PythonGroup/Module 4/PyQt 1.0/PyQt 1.0.py
+++ PythonGroup/Module 4/PyQt 1.0/PyQt 1.0.py
@@ -1,1 +1,34 @@
-# https://docs.google.com/presentation/d/1gqyMy49e3D3A0E9ACEwZGh825fAHHC7Q8yT97-nw4Qw/edit#slide=id.g12dae56b7f6_0_222
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+class NewWin(QMainWindow):
+    def __init__(self):
+        super(NewWin,self).__init__()
+        self.counter=0
+        self.setGeometry(200,200,300,300)
+        self.setWindowTitle("Counter")
+        
+        
+
+    def CreateLabel(self,x=50,y=50):
+        self.newLabel=QtWidgets.QLabel(self)
+        self.newLabel.setText("0")
+        self.newLabel.Move(x,y)
+    
+    def CreateButton(self,x=50,y=50):
+        self.newButton=QtWidgets.QPushButton(self)
+        self.newButton.setText("CLick")
+        self.newButton.Move(x,y)
+        self.newButton.clicked.connect(self.CounterFunc)
+
+
+    def CounterFunc(self):
+        self.counter+=1
+        self.newLabel.setText(str(self.counter))
+
+app=QApplication(sys.argv)
+window=NewWin()
+window.show()
+
+sys.exit(app.exec_())
