5430ad75ef42f4aa80d803c9fb08c14a1ecee2b3
alkihuri <alkihuri@gmail.com>
2022-05-30 17:57:43 +0300

    template

--- PythonGroup/Module 4/PyQt 1.0/PyQt 1.0.py
+++ PythonGroup/Module 4/PyQt 1.0/PyQt 1.0.py
@@ -1,1 +1,2 @@
-# https://docs.google.com/presentation/d/1gqyMy49e3D3A0E9ACEwZGh825fAHHC7Q8yT97-nw4Qw/edit#slide=id.g12dae56b7f6_0_222
+#pyqt 1.0 
+# https://docs.google.com/presentation/d/1gqyMy49e3D3A0E9ACEwZGh825fAHHC7Q8yT97-nw4Qw/edit?usp=sharing
